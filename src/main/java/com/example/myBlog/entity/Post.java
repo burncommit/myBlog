5373b970e03dbb6effe1dfb9167b2eb3a9bd5ac0
sana <burncommit@naver.com>
2023-11-06 17:30:18 +0900

    첫번째 커밋

---
+++ src/main/java/com/example/myBlog/entity/Post.java
@@ -0,0 +1,13 @@
+package com.example.myBlog.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Post {
+}
---
+++ src/main/java/com/example/myBlog/repository/PostRepository.java
@@ -0,0 +1,6 @@
+package com.example.myBlog.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository {
+}
